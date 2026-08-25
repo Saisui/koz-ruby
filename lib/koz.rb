b39aed1a9492220c52194ff00b3f2de9e0807112
彩穂 <37037844+Saisui@users.noreply.github.com>
2026-08-25 19:11:03 +0800

    init

---
+++ lib/koz.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "koz/version"
+
+module Koz
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/koz/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Koz
+  VERSION = "0.1.0"
+end
